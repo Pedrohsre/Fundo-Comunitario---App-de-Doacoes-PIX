215de1f2962e0dece93ec84a41afe3013a40b2a8
Pedro R. <117690162+Pedrohsre@users.noreply.github.com>
2026-07-07 18:35:32 -0300

    Update META_MENSAL value to 95.90

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 CHAVE_PIX       = "bc01cf21-b50b-4285-b880-825822031cf3"          # CPF, CNPJ, e-mail, telefone ou chave aleatória
 NOME_RECEBEDOR  = "Pedro R"    # max 25 caracteres
 CIDADE_PIX      = "teste"            # max 15 caracteres
-META_MENSAL     = 1                # valor total que precisa ser arrecadado
+META_MENSAL     = 95.90                # valor total que precisa ser arrecadado
 MES_REFERENCIA  = "Julho/2026"           # aparece no cabeçalho
 # ──────────────────────────────────────────────
 
